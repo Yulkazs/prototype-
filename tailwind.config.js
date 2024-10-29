6f3acaf3885a4253f47b0bf5b60141f3c5fd9394
Yulkaz <yulkazs@outlook.com>
2024-10-29 15:03:33 +0100

    Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./public/**/*.{html,js}", // Adjust this based on where your HTML files are
+    "./src/**/*.{js,jsx,ts,tsx}", // Include your JS/TS files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
